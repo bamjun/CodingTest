580b34b988cd8015d880591c59fb39cf4f2d0137
bamjun <khy5116@naver.com>
2021-02-27 11:07:49 +0900

    Update 아픈 동물 찾기.sql

--- 아픈 동물 찾기.sql
+++ 아픈 동물 찾기.sql
@@ -27,4 +27,4 @@ A381217	Cherokee
 */
 
 -- 코드를 입력하세요
-SELECT animal_id, name from animal_ins where intake_condition like 'sick' order by animal_id
+SELECT animal_id, name from animal_ins where intake_condition like 'Sick' order by animal_id asc
