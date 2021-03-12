2d5f30c0e5ee9bf8896909496a835bb11978a31d
bamjun <khy5116@naver.com>
2021-03-13 00:05:45 +0900

    Update 스킬트리.py

--- 스킬트리.py
+++ 스킬트리.py
@@ -29,6 +29,22 @@ skill	skill_trees	return
 """
 
 def solution(skill, skill_trees):
+    skill = list(skill)
+    count = len(skill_trees)
+    for tree in skill_trees:
+        index=[]
+        tree = list(tree)
+        for skill_order in skill:
+            if skill_order in tree:
+                index.append(tree.index(skill_order))
+            else:
+                index.append(len(tree))
+        if sorted(index)!=index:
+            count-=1
+    return count
+
+
+def solution(skill, skill_trees):
     answer = 0
 
     for skills in skill_trees:
