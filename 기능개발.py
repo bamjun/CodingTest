b269353cd28b89de3f1e55bd7b591dcac0ff8ce4
bamjun <khy5116@naver.com>
2021-03-13 19:01:48 +0900

    Update 기능개발.py

--- 기능개발.py
+++ 기능개발.py
@@ -32,6 +32,18 @@ progresses	speeds	return
 ※ 공지 - 2020년 7월 14일 테스트케이스가 추가되었습
 
 """
+def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
+
+
+
+
 
 
 def solution(z1, z2):
