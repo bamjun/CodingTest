de798c9162a90507c0d60e223708ad5a6baa8d54
bamjun <khy5116@naver.com>
2021-05-04 21:24:27 +0900

    Update 나머지 한 점.py

--- 나머지 한 점.py
+++ 나머지 한 점.py
@@ -37,3 +37,9 @@ def solution(v):
         zb = 0
 
     return answer
+
+def solution(v):
+    answer = []
+    answer.append(v[0][0]^v[1][0]^v[2][0])
+    answer.append(v[0][1]^v[1][1]^v[2][1])
+    return answer
