8599af5ccf3f37b0d324e98a6aabc637975b8ffa
bamjun <khy5116@naver.com>
2024-07-19 15:00:54 +0900

    [Bronze V] Title: A+B, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.rs
+++ 백준/Bronze/1000. A＋B/A＋B.rs
@@ -3,12 +3,12 @@ use std::io;
 fn main() {
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin().read_line(&mut input).expect("n");
 
     let numbers: Vec<i32> = input
         .trim()
         .split_whitespace()
-        .map(|num| num.parse().expect("Please enter a number"))
+        .map(|num| num.parse().expect("n"))
         .collect();
 
     let sum = numbers[0] + numbers[1];
