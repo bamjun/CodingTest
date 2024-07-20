5896ac5b87ed709ec2d1e66b0ac98bf9e1bfd813
bamjun <khy5116@naver.com>
2024-07-20 11:51:24 +0900

    [Bronze V] Title: 나머지, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.rs
+++ 백준/Bronze/10430. 나머지/나머지.rs
@@ -2,19 +2,17 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let numbers: Vec<i32> = input.trim().split_whitespace()
-                                 .map(|x| x.parse().expect("Not an integer"))
-                                 .collect();
-
-    if numbers.len() == 3 {
-        let (a, b, c) = (numbers[0], numbers[1], numbers[2]);
-        
-        println!("{}", (a + b) % c);
-        println!("{}", ((a % c) + (b % c)) % c);
-        println!("{}", (a * b) % c);
-        println!("{}", (a % c) * (b % c) % c);
-    } else {
-        eprintln!("Please input exactly three integers");
-    }
+    io::stdin().read_line(&mut input).expect("N");
+    
+    let numbers: Vec<i32> = input.split_whitespace().map(|N| N.parse().expect("N")).collect();
+    
+    let A = numbers[0];
+    let B = numbers[1];
+    let C = numbers[2];
+    
+    println!("{}", (A+B)%C);
+    println!("{}", ((A%C) + (B%C))%C);
+    println!("{}", (A*B)%C);
+    println!("{}", ((A%C)*(B%C))%C);
+    
 }
