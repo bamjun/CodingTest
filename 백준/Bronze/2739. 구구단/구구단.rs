9ae0097ace3d97449b726ce08008ac5fcced3b87
bamjun <khy5116@naver.com>
2024-07-24 13:01:42 +0900

    [Bronze V] Title: 구구단, Time: 4 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.rs
+++ 백준/Bronze/2739. 구구단/구구단.rs
@@ -5,7 +5,7 @@ fn main() {
     io::stdin().read_line(&mut input).expect("failed to read line");
     let n: u32 = input.trim().parse().expect("failed to parse number");
     
-    for i in 1..=9 {
+    for i in 1..10 {
         println!("{} * {} = {}", n, i, n*i);
     }
 }
