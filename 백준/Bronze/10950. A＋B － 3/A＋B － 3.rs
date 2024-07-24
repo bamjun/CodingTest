c36b00fdd08df9be5a17351c01c4f57913172fed
bamjun <khy5116@naver.com>
2024-07-24 13:40:54 +0900

    [Bronze V] Title: A+B - 3, Time: 8 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.rs
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.rs
@@ -8,7 +8,7 @@ fn main() {
     for i in 0..n {
         let mut input2 = String::new();
         io::stdin().read_line(&mut input2).expect("failed to read line");
-        let numbers: Vec<i32> = input2.trim().split_whitespace().map(|O| O.parse().expect("failed")).collect();
+        let numbers: Vec<u8> = input2.trim().split_whitespace().map(|O| O.parse().expect("failed")).collect();
         
         println!("{}", numbers[0] + numbers[1]);
     }
