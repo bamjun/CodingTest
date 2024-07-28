801d0d5fa67311967b39fe8286f11c8dc71a80f7
bamjun <khy5116@naver.com>
2024-07-29 01:49:06 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.rs
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.rs
@@ -1,24 +1,24 @@
 use std::io::{self, BufRead, BufWriter, Write};
 
 fn main() {
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-    let mut reader = stdin.lock().lines();
-    let mut writer = BufWriter::new(stdout.lock());
-
+    let mut reader = io::stdin().lock().lines();
+    let mut writer = BufWriter::new(io::stdout().lock());
+    
     while let Some(Ok(line)) = reader.next() {
-        let nums: Vec<i32> = line.split_whitespace()
-                                 .map(|s| s.parse().expect("Failed to parse number"))
-                                 .collect();
+        let nums: Vec<i32> = line.split_whitespace().map(|I| I.parse().unwrap()).collect();
         let a = nums[0];
         let b = nums[1];
+
 
         if a == 0 && b == 0 {
             break;
         }
+        let sum = a + b;
+        writeln!(writer, "{}", sum).unwrap();
 
-        writeln!(writer, "{}", a + b).expect("Failed to write to buffer");
     }
 
-    writer.flush().expect("Failed to flush buffer");
+    
+    writer.flush().unwrap();
+    
 }
