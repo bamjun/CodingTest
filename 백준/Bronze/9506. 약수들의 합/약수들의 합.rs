300e6829a8ea86759e0f4fc5bd8c926c82eb505c
bamjun <khy5116@naver.com>
2024-07-30 02:42:05 +0900

    [Bronze I] Title: 약수들의 합, Time: 4 ms, Memory: 13216 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.rs
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.rs
@@ -1,20 +1,17 @@
 use std::io::{self, BufRead, BufWriter, Write};
 
 fn main() {
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-    let mut reader = stdin.lock().lines();
-    let mut writer = BufWriter::new(stdout.lock());
-
+    let mut reader = io::stdin().lock().lines();
+    let mut writer = BufWriter::new(io::stdout().lock());
+    
     while let Some(Ok(line)) = reader.next() {
         let n: i32 = line.trim().parse().unwrap();
         if n == -1 {
             break;
         }
-        
         let mut divisors: Vec<i32> = Vec::new();
         
-        for i in 1..n { 
+        for i in 1..n {
             if n % i == 0 {
                 divisors.push(i);
             }
@@ -23,10 +20,7 @@ fn main() {
         let sum: i32 = divisors.iter().sum();
         
         if sum == n {
-            let divisors_str: String = divisors.iter()
-                                               .map(|d| d.to_string())
-                                               .collect::<Vec<String>>()
-                                               .join(" + ");
+            let divisors_str: String = divisors.iter().map(|O| O.to_string()).collect::<Vec<String>>().join(" + ");
             writeln!(writer, "{} = {}", n, divisors_str).unwrap();
         } else {
             writeln!(writer, "{} is NOT perfect.", n).unwrap();
