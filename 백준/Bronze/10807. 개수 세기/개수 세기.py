124a7300c53c99e545772a1c82e7f5f85116ef7b
bamjun <khy5116@naver.com>
2025-01-21 23:39:04 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,11 +1,12 @@
 import sys
 
-def check_number_of_v(nums, v):
-    return nums.count(v)
+found = 0
+num = int(sys.stdin.readline().strip())  # 정수로 변환
+numlist = sys.stdin.readline().split()  # 리스트로 변환
+findNum = sys.stdin.readline().strip()  # 오른쪽 공백 제거
 
+for i in range(num):  # num을 정수로 사용
+    if numlist[i] == findNum:  # 문자열 비교
+        found += 1
 
-if __name__ == '__main__':
-    inputs = sys.stdin.read().splitlines()
-    nums = list(map(int, inputs[1].split()))
-    v = int(inputs[2])
-    print(check_number_of_v(nums, v))
+print(found)  # 정수는 str 변환 없이 출력 가능
