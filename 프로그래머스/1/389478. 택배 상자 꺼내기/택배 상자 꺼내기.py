eb6b92355fa54b0f6406f7f1a5d1e965ac485918
bamjun <khy5116@naver.com>
2025-03-27 01:51:33 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,26 +1,7 @@
-def solution(n, w, num):
-    columns = [[] for _ in range(w)]
-    current_box = 1
-    layer = 0
+def solution(n, w, num): 
+    return sum((
+        ((b - 1) % w if ((b - 1) // w) % 2 == 0 else w - 1 - ((b - 1) % w))
+        ==
+        ((num - 1) % w if ((num - 1) // w) % 2 == 0 else w - 1 - ((num - 1) % w))
+    ) for b in range(num + 1, n + 1)) + 1
 
-    while current_box <= n:
-        if layer % 2 == 0:  # 왼->오른
-            for c in range(w):
-                if current_box > n:
-                    break
-                columns[c].append(current_box)
-                current_box += 1
-        else:               # 오른->왼
-            for c in range(w-1, -1, -1):
-                if current_box > n:
-                    break
-                columns[c].append(current_box)
-                current_box += 1
-        layer += 1
-
-    for c in range(w):
-        if num in columns[c]:
-            idx = columns[c].index(num)
-            return len(columns[c]) - idx
-
-    return 0  # 기본적으로 도달하지 않음
