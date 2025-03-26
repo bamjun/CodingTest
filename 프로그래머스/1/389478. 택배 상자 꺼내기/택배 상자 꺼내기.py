871f0ff8e5c7c2a8c9b0721593e31ec54cc717b2
bamjun <khy5116@naver.com>
2025-03-27 01:52:35 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,7 +1,1 @@
-def solution(n, w, num): 
-    return sum((
-        ((b - 1) % w if ((b - 1) // w) % 2 == 0 else w - 1 - ((b - 1) % w))
-        ==
-        ((num - 1) % w if ((num - 1) // w) % 2 == 0 else w - 1 - ((num - 1) % w))
-    ) for b in range(num + 1, n + 1)) + 1
-
+solution = lambda n,w,num: sum((((b-1) % w if ((b-1)//w) % 2 == 0 else w-1 - ((b-1) % w)) == ((num-1) % w if ((num-1)//w) % 2 == 0 else w-1 - ((num-1) % w))) for b in range(num+1, n+1)) + 1
